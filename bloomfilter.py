3ef137ccaea92f657be98bd381c6e5f1dd631a2b
Ahazy <haydena@oregonstate.edu>
2018-10-26 21:57:47 -0700

    Added finishing touches.  Tested and it works.

--- bloomfilter.py
+++ bloomfilter.py
@@ -19,6 +19,17 @@ class BloomFilter:
             self.bit_array[hashed] = True
         return self
 
+    # Function to check if given password is possibly in the filter or definitely not not in the filter
+    def contains(self, item):
+        in_filter = True
+        hash_function = ['sha512', 'sha384', 'sha256', 'sha224', 'md5']
+        for i in range(self.number_hashes):
+            hasher = getattr(hashlib, hash_function[i])
+            hashed = int(hasher(item.encode('utf-8')).hexdigest(), 16) % self.size
+            if not self.bit_array[hashed]:
+                in_filter = False
+        return in_filter
+
 
 # Function to parse input and bad password list
 def parser(any_file):
@@ -30,6 +41,18 @@ def parser(any_file):
     return parsed
 
 
+# Function to write to output3.txt and output5.txt
+def write_output(bloom_filter, input_pws, output_file):
+    file = open(output_file, 'w')  # Open file
+    for pw in input_pws:
+        if bloom_filter.contains(pw):
+            file.write('maybe\n')
+        else:
+            file.write('no\n')
+    file.close()  # Close file
+
+
+# Everything is done within main and the BloomFilter class :) !
 def main():
     # Assign command line arguments to variables
     out3 = sys.argv[6]
@@ -53,5 +76,10 @@ def main():
         bloom_filter5.add(pw)
 
     # Send the input passwords to be tested against the bloom filters and then written to output files
+    print("Comparing input passwords with the Bloom Filters!")
+    write_output(bloom_filter3, input_list, out3)
+    write_output(bloom_filter5, input_list, out5)
+    print("Check output3.txt and output5.txt for results!")
+
 
 main()
